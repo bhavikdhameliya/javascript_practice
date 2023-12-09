8e8d331cdb8cf3640bdd6b468e13d6109161986d
bhavik dhameliya <bhavikdhameliya20@gmail.com>
2023-12-09 11:11:31 +0530

    first commit

---
+++ ex1.js/tsak1.js
@@ -0,0 +1,20 @@
+// console.log("hello");
+
+// const product = {
+//     "name": "pen" ,
+//     "reting": 4,
+//      "is deal": "false",
+//      "price":40
+// }
+// console.log(product);
+
+const insta = {
+    username : "@blackcobra",
+    isfollow : "yes",
+    post : 5,
+    follwers : 56789,
+    following : 456,
+    name : "bhavik",
+    desciption : " hi i am bhavik dhameliya (black cobra)"
+};
+console.log(insta);
