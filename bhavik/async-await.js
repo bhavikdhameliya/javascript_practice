7fe1c8bc0d09b9a3ac339bac047967f4d6f3418a
bhavik dhameliya <bhavikdhameliya20@gmail.com>
2024-04-10 01:01:44 +0530

    hi

--- bhavik/async-await.js
+++ bhavik/async-await.js
@@ -47,7 +47,7 @@
 //                 console.log("error bhavik");
 //             }
 //         } catch (error) {
-            
+
 //         }
 //     }, 4000);
 // }
@@ -65,7 +65,7 @@
 //     await api();
 // }
 // getData();
- 
+
 // function getData(dataId){
 //     return new Promise((resolve, reject) => {
 //         setTimeout(() => {
@@ -77,8 +77,53 @@
 // async function detAllData(){
 //     await getData(1);
 //     await getData(2);
-//     await getData(3);
+//     await getData(3); 
 // } 
 // detAllData();
 
 
+// function getData(detaid){
+//     return new Promise((resolve, reject) => {
+//        setTimeout(() => {
+//         console.log("data", detaid);
+//         resolve();
+//        }, 2000); 
+//     })
+// }
+// async function getAllData(){
+//             getData(1);
+//             getData(2);
+//      await  getData(3);
+//             getData(4);
+//      await  getData(5);
+//             getData(6);
+// }
+// getAllData();
+// console.log(getData(7));
+// console.log(getData(8));
+
+
+
+function tempfun() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let count = 5;
+            if (count % 5 === 0) {
+                resolve("multipel are");
+            } else {
+                reject("not multipel")
+            }
+        }, 2000);
+    })
+}
+async function asyncFun() {
+    const result = await tempfun();
+    return result
+}
+asyncFun().then((result) => {
+    console.log("then block");
+    console.log(result);
+}).catch((err) => {
+    console.log("catch block");
+    console.log(err);
+})
